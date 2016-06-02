ef08844ec1f94251dda78fd7c1019ac7826766ce
trExie <slavagor.v@hotmail.com>
2016-06-02 13:42:07 +0300

    Создан класс и приложение.

---
+++ VKmessageApp/VKmessageApp/MainPage.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.Phone.UI.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону приложения веб-представления см. по адресу http://go.microsoft.com/fwlink/?LinkID=391641
+
+namespace VKmessageApp
+{
+    public sealed partial class MainPage : Page
+    {
+        // TODO: замените своим URL-адресом.
+        private static readonly Uri HomeUri = new Uri("ms-appx-web:///Html/index.html", UriKind.Absolute);
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// Вызывается перед отображением этой страницы во фрейме.
+        /// </summary>
+        /// <param name="e">Данные события, описывающие, каким образом была достигнута эта страница.
+        /// Этот параметр обычно используется для настройки страницы.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            WebViewControl.Navigate(HomeUri);
+
+            HardwareButtons.BackPressed += this.MainPage_BackPressed;
+        }
+
+        /// <summary>
+        /// Вызывается переходе с данной страницы.
+        /// </summary>
+        /// <param name="e">Данные о событиях, описывающие переход на эту страницу.</param>
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            HardwareButtons.BackPressed -= this.MainPage_BackPressed;
+        }
+
+        /// <summary>
+        /// Переопределение нажатия кнопки "Назад" для навигации по обратному стеку веб-представления, а не приложения.
+        /// </summary>
+        private void MainPage_BackPressed(object sender, BackPressedEventArgs e)
+        {
+            if (WebViewControl.CanGoBack)
+            {
+                WebViewControl.GoBack();
+                e.Handled = true;
+            }
+        }
+
+        private void Browser_NavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
+        {
+            if (!args.IsSuccess)
+            {
+                Debug.WriteLine("Navigation to this page failed, check your internet connection.");
+            }
+        }
+
+        /// <summary>
+        /// Переход вперед по журналу веб-представления.
+        /// </summary>
+        private void ForwardAppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (WebViewControl.CanGoForward)
+            {
+                WebViewControl.GoForward();
+            }
+        }
+
+        /// <summary>
+        /// Переход на первоначальную домашнюю страницу.
+        /// </summary>
+        private void HomeAppBarButton_Click(object sender, RoutedEventArgs e)
+        {
+            WebViewControl.Navigate(HomeUri);
+        }
+    }
+}
